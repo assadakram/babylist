1d72304682c149838a7d67571c04efda73fc9be0
assadakram <assad5724@gmail.com>
2021-11-29 23:46:42 +0500

    issue resolved

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -55,8 +55,7 @@ export function Header() {
         >
           <div className="px-2 pt-2 pb-3 w-full bg-white ">
             <Link href="/">
-              <a className="bg-white block px-3 py-2 rounded-md text-base font-semibold text-left"
-              >
+              <a className="bg-white block px-3 py-2 rounded-md text-base font-semibold text-left">
                 Log in
               </a>
             </Link>
